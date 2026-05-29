96a1af2f3042274ae17968cba7b7033944764177
Victor Cluet <victor.cluet01@estudiant.upf.edu>
2026-05-29 11:38:48 +0200

    fixing format

--- test/test.c
+++ test/test.c
@@ -1,11 +1,1 @@
 #include "sample_lib_test.c"
-#include "utils.h"
-#include <stdio.h>
-
-int main() {
-  {
-    // Call all test modules you want to run here
-    sample_lib_test();
-  }
-  allsuccess();
-}
